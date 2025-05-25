9cd017a613887b779f9b0a520b51fd6f58012151
Balbino-teste <balbinojteste@gmail.com>
2025-05-24 21:32:58 -0300

    Iniciando uma landing page de tenis

---
+++ tenis-react/src/componentes/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+
+const NavbarMenu = [
+    {id: 1, title: "New Release", link:"/"},
+    {id: 1, title: "Man", link:"#"},
+    {id: 1, title: "Womam", link:"#"},
+    {id: 1, title: "Kid", link:"#"},
+    {id: 1, title: "Shoes", link:"#"},
+];
+
+export default NavbarMenu;
