c457e3f7c57df50ca5005fb4db2b4c7cd0f18006
Balbino-teste <balbinojteste@gmail.com>
2025-05-29 20:12:21 -0300

    Estruturando a página home
    Nessa seção estou criando a estrutura home.

--- Balbino-JS/src/componentes/Home.jsx
+++ Balbino-JS/src/componentes/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi"
 
 
 
@@ -6,6 +7,19 @@ const Home = () => {
     return (
         <div className="home-container">
             <Navbar />
+
+            <div className="home-banner-container">
+                <div className="home-bannerimage-container">
+                  
+                </div>
+                <div className="home-text-section">
+                    <h1 className="primary-heading">Seu tenis favorito no seus peis</h1>
+                    <p className="primary-text">
+                        Diversos modelos para sua escolha e momentos apropriados. Escolha já o seu!
+                    </p>
+                    <button className="sencondary-button">Fale conosco <FiArrowRight /></button>
+                </div>
+            </div>
         </div>
     )
 }
