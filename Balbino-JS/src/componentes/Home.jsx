4824a50c390ad2cdc07b05a0fda1eefcaa677567
Balbino-teste <balbinojteste@gmail.com>
2025-05-30 20:54:46 -0300

    Adicionei a imagem de fundo e apliquei o css com algumas mudança e correções

--- Balbino-JS/src/componentes/Home.jsx
+++ Balbino-JS/src/componentes/Home.jsx
@@ -1,37 +1,38 @@
-import Navbar from "./Navbar"
-import { FiArrowRight } from "react-icons/fi"
-import BannerImage from "../assets/tinis-azul.png"
-
-
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+import BannerBackground from "../assets/banner-sem-fundo.png";
+import BannerImage from "../assets/sapato-social-sem-fundo-azul.png";
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <Navbar />
+  return (
+    <div className="home-container">
+      <Navbar />
 
-            <div className="home-banner-container">
-                <div className="home-bannerimage-container">
-                  
-                </div>
-                <div className="home-text-section">
-                    <h1 className="primary-heading">Seu tenis favorito no seus peis</h1>
-                    <p className="primary-text">
-                        Diversos modelos para sua escolha e momentos apropriados. Escolha já o seu!
-                    </p>
-                    <button className="sencondary-button">Fale conosco <FiArrowRight /></button>
-
-                    <p className="primary-text">
-                        Venha conhecer nossa loja um ambiente climatizado e aconchegante com várias opeçoes para sua escolha.
-                    </p>
-                </div>
-                <div className="home-image-section">
-                    <div className="home-primary-image">
-                        <img src={BannerImage} alt="Tenis" />
-                    </div>
-                </div>
-            </div>
+      <div className="home-banner-container">
+        <div className="home-bannerimage-container">
+          <img src={BannerBackground} alt="imagem do fundo da home" />
         </div>
-    )
-}
- 
+        <div className="home-text-section">
+          <h1 className="primary-heading">Seu tênis favorito nos seus pés</h1>
+          <p className="primary-text">
+            Diversos modelos para sua escolha e momentos apropriados. Escolha já o seu!
+          </p>
+          <button className="secondary-button">
+            Fale conosco <FiArrowRight />
+          </button>
+
+          <p className="primary-text">
+            Venha conhecer nossa loja, um ambiente climatizado e aconchegante com várias opções para sua escolha.
+          </p>
+        </div>
+        <div className="home-image-section">
+          <div className="home-primary-image">
+            <img src={BannerImage} alt="Tênis" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
