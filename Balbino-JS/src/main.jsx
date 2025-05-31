f680cad9969defe9ac03490492b7551555fddffd
Balbino-teste <balbinojteste@gmail.com>
2025-05-30 21:34:18 -0300

    adicion  a estrutura sobre nós
    Nessa seções foi criado a estrutura da parte onde fala sobre a loja.

--- Balbino-JS/src/main.jsx
+++ Balbino-JS/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import Home from "./componentes/Home"
+import About from './componentes/About.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
      <Home />
+     <About />
   </StrictMode>,
 )
